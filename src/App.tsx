975f6943a51d0585984b20bb57296ee6fb32cb7b
Ankit Shukla <ankit2099shukla@gmail.com>
2023-10-18 00:39:49 +0530

    Add dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import Navbar from "./components/Navbar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -54,24 +55,27 @@ const App = () => {
       </Show>
 
       <GridItem area={"main"}>
-        <HStack spacing={5} paddingLeft={3} marginBottom={5}>
-          <PlatformSelector
-            onSelectPlatform={(platform) => {
-              setGameQuery((prevData) => {
-                return { ...prevData, platform };
-              });
-            }}
-            selectedPlatform={gameQuery.platform}
-          />
-          <SortSelector
-            onSelectSort={(sortValue) => {
-              setGameQuery((prevData) => {
-                return { ...prevData, sortOrder: sortValue };
-              });
-            }}
-            sortOrder={gameQuery.sortOrder}
-          />
-        </HStack>
+        <Box paddingLeft={3}>
+          <GameHeading gameQuery={gameQuery} />
+          <HStack spacing={5} marginBottom={5}>
+            <PlatformSelector
+              onSelectPlatform={(platform) => {
+                setGameQuery((prevData) => {
+                  return { ...prevData, platform };
+                });
+              }}
+              selectedPlatform={gameQuery.platform}
+            />
+            <SortSelector
+              onSelectSort={(sortValue) => {
+                setGameQuery((prevData) => {
+                  return { ...prevData, sortOrder: sortValue };
+                });
+              }}
+              sortOrder={gameQuery.sortOrder}
+            />
+          </HStack>
+        </Box>
 
         <GameGrid gameQuery={gameQuery} />
       </GridItem>
