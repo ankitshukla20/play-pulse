c49309af751ac1b721f2b2206bd2f6c35502184d
Ankit Shukla <ankit2099shukla@gmail.com>
2023-10-18 09:22:34 +0530

    Clean up the game cards

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   HStack,
+  Heading,
   Image,
   List,
   ListItem,
@@ -19,29 +20,34 @@ const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   if (error) return null;
   if (isLoading) return <Spinner />;
   return (
-    <List>
-      {genres.map((g) => (
-        <ListItem key={g.id} paddingY={"6px"}>
-          <HStack>
-            <Image
-              boxSize={"32px"}
-              borderRadius={"8px"}
-              src={g.image_background}
-            />
-            <Button
-              fontWeight={g.id === selectedGenre?.id ? "bold" : "normal"}
-              variant={"link"}
-              fontSize={"lg"}
-              onClick={() => {
-                onSelectGenre(g);
-              }}
-            >
-              {g.name}
-            </Button>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      <Heading fontSize={"2xl"} marginBottom={3}>
+        Genres
+      </Heading>
+      <List>
+        {genres.map((g) => (
+          <ListItem key={g.id} paddingY={"6px"}>
+            <HStack>
+              <Image
+                boxSize={"32px"}
+                borderRadius={"8px"}
+                src={g.image_background}
+              />
+              <Button
+                fontWeight={g.id === selectedGenre?.id ? "bold" : "normal"}
+                variant={"link"}
+                fontSize={"lg"}
+                onClick={() => {
+                  onSelectGenre(g);
+                }}
+              >
+                {g.name}
+              </Button>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
